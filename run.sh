d1f9aa7b62c5d99dd6de871366e0f601a2c32448
tim.liu <tim.liu@vpon.com>
2021-06-14 17:14:56 +0800

    init

---
+++ run.sh
@@ -0,0 +1,3 @@
+
+
+curl --header "Content-Type: application/json" --request POST --header "authorization:NTI1NTY3NTIwNzAzMjUwNDMy.YLjUSA.3yV6re_Kb4aWWeUyXWCewbZKaXE" --data '{"content": "!shop"}' https://discord.com/api/v9/channels/837596301838647326/messages
